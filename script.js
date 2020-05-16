bf674223afd2ef1529dafe57085bddccd2b3718d
Laura Lara <llaraama@uncc.edu>
2020-05-15 22:30:59 -0400

    first version of hw4

---
+++ script.js
@@ -0,0 +1,131 @@
+
+var trig = setInterval(timer,1000);
+
+var counter=0;
+var min=0;
+var sec=60;
+
+function timer(){
+
+sec=--sec;
+
+
+ document.getElementById("output").innerHTML = min+" : "+sec;
+
+ }
+
+
+
+function Quiz(questions) {
+    this.score = 0;
+    this.questions = questions;
+    this.questionIndex = 0;
+
+
+}
+ 
+Quiz.prototype.getQuestionIndex = function() {
+    return this.questions[this.questionIndex];
+}
+ 
+Quiz.prototype.guess = function(answer) {
+    if(this.getQuestionIndex().isCorrectAnswer(answer)) {
+        this.score++;
+        var resultsCorrect=document.createElement("h5");
+        resultsCorrect.textContent="Correct!";
+        document.body.appendChild(resultsCorrect);
+        
+    }else if (this.getQuestionIndex().isNotCorrectAnswer(answer)){
+        var resultsIncorrect=document.createElement("h5");
+        resultsIncorrect.textContent="Incorrect!";
+        document.body.appendChild(resultsIncorrect);
+    }
+ 
+    this.questionIndex++;
+}
+ 
+Quiz.prototype.isEnded = function() {
+    return this.questionIndex === this.questions.length;
+}
+ 
+ 
+function Question(text, choices, answer) {
+    this.text = text;
+    this.choices = choices;
+    this.answer = answer;
+}
+ 
+Question.prototype.isCorrectAnswer = function(choice) {
+    return this.answer === choice;
+}
+
+Question.prototype.isNotCorrectAnswer=function(choice){
+    return this.answer !== choice;
+}
+ 
+function populate() {
+    if(quiz.isEnded()) {
+        showScores();
+    }
+    else {
+        // show question
+        var element = document.getElementById("question");
+        element.innerHTML = quiz.getQuestionIndex().text;
+ 
+        // show options
+        var choices = quiz.getQuestionIndex().choices;
+        for(var i = 0; i < choices.length; i++) {
+            var element = document.getElementById("choice" + i);
+            element.innerHTML = choices[i];
+            guess("btn" + i, choices[i]);
+        }
+ 
+        showProgress();
+    
+    }
+};
+ 
+function guess(id, guess) {
+    var button = document.getElementById(id);
+    button.onclick = function() {
+        quiz.guess(guess);
+        populate();
+    }
+};
+ 
+ 
+function showProgress() {
+    var currentQuestionNumber = quiz.questionIndex + 1;
+    var element = document.getElementById("progress");
+    element.innerHTML = "Question " + currentQuestionNumber + " of " + quiz.questions.length;
+};
+ 
+
+
+function showScores() {
+    var gameOverHTML = "<h1>All Done!</h1>";
+    gameOverHTML += "<h4 id='score'> Your final score: " + quiz.score + "</h4>";
+    gameOverHTML +="<form>Enter Initals:<label class='sr-only'for='inlineFormInputInitalse'>Initals</label><input type='text' class='form-control' id='inlineFormInputInitals' placeholder='LL'></form>"
+    gameOverHTML +="<form method='get' action=file:///C:/Users/laura/first_day_stuff/New%20folder/highscores-page.html><button type ='sumbit'> submit</button></form>"
+    var element = document.getElementById("quiz");
+    element.innerHTML = gameOverHTML;
+};
+
+
+
+
+// create questions here
+var questions = [
+    new Question("Hyper Text Markup Language Stand For?", ["JavaScript", "XHTML","CSS", "HTML"], "HTML"),
+    new Question("Which language is used for styling web pages?", ["HTML", "JQuery", "CSS", "XML"], "CSS"),
+    new Question("Which is not a JavaScript Framework?", ["Python Script", "JQuery","Django", "NodeJS"], "Django"),
+    new Question("Which is used for Connect To Database?", ["PHP", "HTML", "JS", "All"], "PHP"),
+    new Question("Webdevtrick.com is about..", ["Web Design", "Graphic Design", "SEO & Development", "All"], "All")
+
+];
+
+// create quiz
+var quiz = new Quiz(questions);
+ 
+// display quiz
+populate();
